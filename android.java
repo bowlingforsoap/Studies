2b7ff166623a5e795f2f8911d3e330538cbc0770
Strelchenko Vadym <bowlingforsoap@mail.ru>
2016-06-14 22:44:08 +0200

    Update android.java

--- android.java
+++ android.java
@@ -1,3 +1,10 @@
+Dictionary
+==========
+An 'activity' is a single, focused thing that the user can do. Almost all activities interact with the user, so the Activity class takes care of creating a window for you in which you can place your UI with setContentView(View). While activities are often presented to the user as full-screen windows, they can also be used in other ways: as floating windows (via a theme with windowIsFloating set) or embedded inside of another activity (using ActivityGroup).
+
+
+
+
 (res/)R(.java)
 ==============
 Resource Objects
