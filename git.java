b3ca906aa9ae8fbd557b82b86bf6ed3433037e01
Strelchenko Vadym <bowlingforsoap@mail.ru>
2016-11-08 19:40:29 +0100

    add remote

--- git.java
+++ git.java
@@ -1,1 +1,7 @@
+ADD REMOTE
+----/-----
+To add a new remote, use the git remote add command on the terminal, in the directory your repository is stored at.
 
+The git remote add command takes two arguments:
+  A remote name, for example, origin
+  A remote URL, for example, https://github.com/user/repo.git
