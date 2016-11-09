7c050ce03ed237ecd5c26d6742d057021b0033c2
Strelchenko Vadym <bowlingforsoap@mail.ru>
2016-11-09 12:08:07 +0100

    git clone

--- git.java
+++ git.java
@@ -5,3 +5,9 @@ To add a new remote, use the git remote add command on the terminal, in the dire
 The git remote add command takes two arguments:
   A remote name, for example, origin
   A remote URL, for example, https://github.com/user/repo.git
+  
+CLONE
+--/--
+  // my-linux folder has to be empty
+  $ git clone git://git.kernel.org/pub/scm/.../linux.git my-linux
+  $ cd my-linux
